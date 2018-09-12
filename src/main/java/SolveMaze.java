3261fb780998834be37c8b50d6b33ed912725909
Gant04 <43008028+Gant04@users.noreply.github.com>
2018-09-12 12:38:00 -0500

    1. check we can move right 2. check we can move left 3. check we can move forward 4. move back if can't move in any directions

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -36,22 +36,23 @@ public class SolveMaze {
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
             while (maze.isFinished() == false) {
-                maze.turnRight();
-                if (maze.canMove() == true) {
-                    maze.move();
-                } else {
-                    maze.turnLeft();
-                    maze.turnLeft();
-                    if (maze.canMove()== true) {
-                        maze.move();
-                    } else{
-                        maze.turnRight();
-                    }  if (maze.canMove()== true){
-                        maze.turnRight();
-                        maze.turnRight();
-                        maze.move();
-                    }
-                }
+               maze.turnRight();
+               if (maze.canMove()){
+                   maze.move();
+               } else {
+                 maze.turnLeft();
+                 if (maze.canMove()){
+                     maze.move();
+                 } else {
+                   maze.turnLeft();
+                   if (maze.canMove()){
+                       maze.move();
+                   } else {
+                       maze.turnLeft();
+                       maze.move();
+                   }
+                 }
+               }
             }
 
         }
